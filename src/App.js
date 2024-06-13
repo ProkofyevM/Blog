f65ce7ca2407753e9900eca124fd763d99312825
Миша Прокофьев <misaprokofev@MacBook-Air--Misha.local>
2024-06-13 18:37:38 +0300

    Настроить проект

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Div = styled.div`
+	text-align: center;
+`
+
+const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-grav"></i>
+			<div>123</div>
+		</Div>
+	)
 }
 
-export default App;
+export default App
